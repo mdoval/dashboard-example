7ddda5cab24edf2dc8fae2d36ed3f1269340d364
Martinchoe <martindoval@gmail.com>
2024-05-15 22:57:06 -0300

    nav bar

---
+++ app/dashboard/categorias/page.tsx
@@ -0,0 +1,3 @@
+export default function CategoriasPage() {
+    return <h1>Categorias</h1>
+}
--- app/ui/dashboard/dashboard-navbar.tsx
+++ app/ui/dashboard/dashboard-navbar.tsx
@@ -1,13 +1,18 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function DashboardNavBar() {
+  const path = usePathname()
+  console.log(path)
   return(
-    <ul className="menu bg-base-200 w-56">
+    <ul className="menu bg-gray-200 w-56">
       <li>
-        <Link href={"/dashboard/productos"}>Productos</Link>
+        <Link href={"/dashboard/productos"} className={path === "/dashboard/productos"? "active" : ""}>Productos</Link>
       </li>
       <li>
-          <Link href={"/dashboard/categorias"}>Categorias</Link>
+          <Link href={"/dashboard/categorias"} className={path === "/dashboard/categorias"? "active" : ""}>Categorias</Link>
       </li>
     </ul>
   );
