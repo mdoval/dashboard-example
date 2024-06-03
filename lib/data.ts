e1df29a7936a6026952c59ac70760f62c68d8663
Martin Doval <martindoval@gmail.com>
2024-06-03 20:02:58 +0200

    Test Prisma Grafico Circular

--- lib/data.ts
+++ lib/data.ts
@@ -204,7 +204,14 @@ export async function fetchCategoria(id: number) {
 
 export async function fetchProductosXCategoria() {
     try {
-        const resultados = await prisma.$queryRaw`SELECT category.id, COUNT(product.id) as value, category.name as label FROM product  inner join category on product.categoryId = category.id GROUP BY category.name`
+       // const resultados = await prisma.$queryRaw`SELECT category.id, COUNT(product.id) as value, category.name as label FROM product  inner join category on product.categoryId = category.id GROUP BY category.name`
+       const resultados = await prisma.product.findMany(
+        {
+            include: {
+                category: true,
+            }           
+        }
+       )
         console.log(resultados)
         return resultados
     } catch( error ) {
